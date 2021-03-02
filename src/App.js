ebc71c0b1869a6b4af1753117c5ffe05961aaad5
suhel311 <76218258+suhel311@users.noreply.github.com>
2021-03-02 23:09:16 +0600

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       { id: 2, bookName: "The Da Vinci Code", writer: "Dan Brown" },
       { id: 3, bookName: "The Alchemist", writer: "Paulo Coelho" }
     ],
-
+    showBooks: true
   }
 
   changeWithInputState = (event, index) => {
@@ -19,19 +19,23 @@ class App extends Component {
     book.bookName = event.target.value;
     const books = [...this.state.books];
     books[index] = book;
+
     this.setState({ books: books });
   }
 
   deleteBookState = index => {
-    // const books = this.state.books.splice();
+    //const books = this.state.books.slice();
     //const books = this.state.books.map(item => item);
     const books = [...this.state.books];
     books.splice(index, 1);
     this.setState({
       books: books
     });
-
   };
+
+  toggleBooks = () => {
+    this.setState({ showBooks: !this.state.showBooks });
+  }
 
   render() {
     const style = {
@@ -43,24 +47,29 @@ class App extends Component {
 
     //const booksState = this.state.books;
 
-    const books = this.state.books.map((book, index) => {
-      return (
-        <Book
-          bookName={book.bookName}
-          writer={book.writer}
-          delete={() => this.deleteBookState(index)}
-          key={book.id}
-          inputName={(event) => this.changeWithInputState(event, index)}
-        />
-      );
-    });
+    let books = null;
+    if (this.state.showBooks) {
+      books = this.state.books.map((book, index) => {
+        return (
+          <Book
+            bookName={book.bookName}
+            writer={book.writer}
+            delete={() => this.deleteBookState(index)}
+            key={book.id}
+            inputName={(event) => this.changeWithInputState(event, index)}
+          />
+        );
+      });
+    }
 
-    //console.log(this.state);
+
+    //console.log(booksState);
     console.log(books);
+
     return (
       <div className="App">
         <h1 style={style}>Book List</h1>
-
+        <button onClick={this.toggleBooks}>Toggle Books</button>
         {books}
       </div>
     );
